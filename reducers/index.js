494faa2a1e00657f1361a7e991fbb33628290f34
Thiago Dias <thiagoqd@gmail.com>
2018-04-08 19:26:08 -0300

    REMOVE_DECK reducer included

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,9 @@
-import { RECEIVE_DECKS, ADD_DECK, RECEIVE_DECKS_BY_ID, ADD_CARD } from '../actions'
+import { RECEIVE_DECKS, ADD_DECK, RECEIVE_DECKS_BY_ID, ADD_CARD, REMOVE_DECK } from '../actions'
 
 function decks (state = {}, action) {
   switch (action.type) {
     case RECEIVE_DECKS :
       return {
-        ...state,
         ...action.decks,
       }
     case RECEIVE_DECKS_BY_ID:
@@ -17,17 +16,25 @@ function decks (state = {}, action) {
         ...state,
         [action.deckId]: action.deck
       }
-      case ADD_CARD :
-        return {
-          ...state,
-          [action.deckId]: {
-            ...state[action.deckId],
-            questions:[
-              ...state[action.deckId].questions,
-              action.card
-            ]
-          }
+
+    case REMOVE_DECK:
+      var data = Object.assign({}, state)
+      delete data[action.key]
+      
+      return data
+          
+      
+    case ADD_CARD :
+      return {
+        ...state,
+        [action.deckId]: {
+          ...state[action.deckId],
+          questions:[
+            ...state[action.deckId].questions,
+            action.card
+          ]
         }
+      }
     default :
       return state
   }
