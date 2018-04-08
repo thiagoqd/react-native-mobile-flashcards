ba3d4003da3e987a34267ea4e65f7619859f89e3
Thiago Dias <thiagoqd@gmail.com>
2018-04-08 19:27:19 -0300

    Remove deck functionality included

--- components/DeckCover.js
+++ components/DeckCover.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
 import { lightGray, white, primaryDark, secondaryLight } from '../utils/colors'
 import { connect } from 'react-redux'
-import { fetchDecksById } from '../utils/api'
-import { receiveDeckById } from '../actions'
+import { fetchDecksById, fetchRemoveDeck } from '../utils/api'
+import { receiveDeckById, removeDeck } from '../actions'
 import { AppLoading } from 'expo'
-
-
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import Modal from 'react-native-modal'
 
 class DeckCover extends Component {
     state = {
-        ready : false
+        ready : false,
+        isModalVisible: false
     }
 
     static navigationOptions = ({ navigation }) => {
@@ -20,6 +21,19 @@ class DeckCover extends Component {
         return {
             title: `${title}`
         }
+    }
+
+    _toggleModal = () =>
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+
+    submitDelete = () => {
+
+      fetchRemoveDeck(this.props.deck.title)
+      .then(this.setState({ isModalVisible: !this.state.isModalVisible }))
+      .then(this.props.removeDeck(this.props.deck.title)) 
+      .then(this.props.navigation.navigate(
+        'Home'
+        ))    
     }
 
      componentDidMount () {  
@@ -32,11 +46,12 @@ class DeckCover extends Component {
 
   render() {
 
-    const { ready } = this.state 
-    if (ready === false) {
+    const { ready } = this.state  
+    if (ready === false || !this.props.deck) {
         return <AppLoading />
     }  
   
+    const {isModalVisible} = this.state 
 
     return (
         <View style={styles.container}>
@@ -61,6 +76,36 @@ class DeckCover extends Component {
                 <Text style={styles.startQuizBtnTxt}>Start Quiz</Text>
               </View>
           </TouchableOpacity>
+          <TouchableOpacity  onPress={this._toggleModal}>  
+              <View>
+                {
+                  Platform.OS == 'ios' ?
+                  <Ionicons style={styles.trashIcon} size={30} name='ios-trash' /> :
+                  <Ionicons style={styles.trashIcon} size={30} name='md-trash' />
+                }  
+              </View>
+          </TouchableOpacity>
+          <Modal isVisible={isModalVisible}>
+            <View style={styles.modal}>
+              <Text style={styles.title}>Delete Deck?</Text>
+              <View style={styles.modalBody}>
+                <TouchableOpacity onPress={this.submitDelete}>
+                  <View style={styles.confirmBtn}>
+                    <Text style={styles.confirmBtnTxt}>Confirm</Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this._toggleModal}>
+                  <View style={styles.cancelBtn}>
+                    <Text style={styles.cancelBtnTxt}>Cancel</Text> 
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+          <View>
+          
+          </View>
+
         </View> 
     ); 
   } 
@@ -77,8 +122,76 @@ const styles = StyleSheet.create({
       fontSize: 30,
       color: primaryDark
     },
+    modal: {
+      borderRadius: Platform.OS === 'ios' ? 16 : 5,
+      height: 300,
+      marginLeft: 20,
+      marginRight: 20,
+      backgroundColor: white,
+      alignItems: 'center',
+      justifyContent: 'center',
+      shadowRadius: 3,
+      shadowOpacity: 0.8,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      }
+
+    },
+    modalBody: {
+      flexDirection: 'row'
+    },
     bodyText: {
       fontSize: 20,
+      color: secondaryLight
+    },
+    cancelBtn: {
+      borderRadius: Platform.OS === 'ios' ? 16 : 5,
+      height: 50,
+      marginTop: 50,
+      marginLeft: 10, 
+      paddingLeft: 20, 
+      paddingRight: 20,
+      borderWidth: 1,
+      borderColor: primaryDark,
+      alignItems: 'center',
+      justifyContent: 'center',
+      shadowRadius: 3,
+      shadowOpacity: 0.8,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      }
+    },
+    cancelBtnTxt:{
+        fontSize: 20,
+        color: primaryDark
+    },
+    confirmBtn:{
+        borderRadius: Platform.OS === 'ios' ? 16 : 5,
+        height: 50,
+        marginTop: 50,
+        paddingLeft: 20, 
+        paddingRight: 20,
+        backgroundColor: primaryDark,
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        }
+    },
+    confirmBtnTxt:{
+        fontSize: 20,
+        color: white
+    },
+    trashIcon: {
+      marginTop: 30,
       color: secondaryLight
     },
     addCardBtn: {
@@ -140,6 +253,7 @@ function mapStateToProps (state, props) {
   const mapDispatchToProps = (dispatch) => {
     return {
       getDeck: (deck, deckId) => dispatch(receiveDeckById(deck, deckId)),
+      removeDeck: (key) => dispatch(removeDeck(key)),
     }
   }
   
